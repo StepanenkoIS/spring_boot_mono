ffcf9d2e5ebd4c07098d268fb8b8c1f54ddf70a9
StepanenkoIS <Citizen(18)>
2021-06-13 16:48:20 +0400

    gitignore, new app

---
+++ src/main/java/is/springbootmono/SpringBootMonoApplication.java
@@ -0,0 +1,13 @@
+package is.springmono;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMonoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMonoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/is/springmono/SpringMonoApplicationTests.java
@@ -0,0 +1,13 @@
+package is.springmono;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMonoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
